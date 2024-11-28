0e46a4f30ab6e8331d08bf69da39f6b3c0e52a37
Ishaan Takrani <ishaantakrani@gmail.com>
2024-11-28 10:46:08 -0500

    readme, resolved dependency issuess

--- 28Nov/src/main/java/frc/robot/Robot.java
+++ 28Nov/src/main/java/frc/robot/Robot.java
@@ -11,19 +11,64 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
+// CTRE
+
+  import edu.wpi.first.wpilibj.Joystick;
+  import edu.wpi.first.wpilibj.TimedRobot;
+  import edu.wpi.first.wpilibj.Timer;
+  import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+  import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+
+//REV
+
+  // import com.revrobotics.CANSparkMax;
+  // import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+  // import edu.wpi.first.wpilibj.TimedRobot;
+  // import edu.wpi.first.wpilibj.Timer;
+  // import edu.wpi.first.wpilibj.XboxController;
+  // import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+ 
+
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
  * the package after creating this project, you must also update the manifest file in the resource
  * directory.
  */
-public class Robot extends TimedRobot {
-  private final PWMSparkMax m_leftDrive = new PWMSparkMax(0);
-  private final PWMSparkMax m_rightDrive = new PWMSparkMax(1);
-  private final DifferentialDrive m_robotDrive =
-      new DifferentialDrive(m_leftDrive::set, m_rightDrive::set);
-  private final XboxController m_controller = new XboxController(0);
-  private final Timer m_timer = new Timer();
+
+
+// public class Robot extends TimedRobot {
+
+//   // PWM
+//   private final PWMSparkMax m_leftDrive = new PWMSparkMax(0);
+//   private final PWMSparkMax m_rightDrive = new PWMSparkMax(1);
+//   private final DifferentialDrive m_robotDrive =
+//       new DifferentialDrive(m_leftDrive::set, m_rightDrive::set);
+//   private final XboxController m_controller = new XboxController(0);
+//   private final Timer m_timer = new Timer();
+
+  // CTRE: includes class definition
+  /*
+   public class Robot extends TimedRobot {
+   private final WPI_TalonFX m_leftDrive = new WPI_TalonFX(1);
+   private final WPI_TalonFX m_rightDrive = new WPI_TalonFX(2);
+   private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftDrive, m_rightDrive);
+   private final Joystick m_stick = new Joystick(0);
+   private final Timer m_timer = new Timer();
+   */
+   
+
+   // REV: includes class definition
+  
+    public class Robot extends TimedRobot {
+    private final CANSparkMax m_leftDrive = new CANSparkMax(1, MotorType.kBrushless);
+    private final CANSparkMax m_rightDrive = new CANSparkMax(2, MotorType.kBrushless);
+    private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftDrive, m_rightDrive);
+    private final XboxController m_controller = new XboxController(0);
+    private final Timer m_timer = new Timer();
+    
 
   public Robot() {
     SendableRegistry.addChild(m_robotDrive, m_leftDrive);
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-
 > Ishaan Takrani
+
+## Installing Vendor Libraries
+
+To resolve issues with dependencies, such as, if an import
+cannot be resolved, a library may need to be added.
+
+To add a library, open the command pallete in WPIlib's VSCode, and navigate to the following:
+Manage Vendor Libraries > Install new libraries (online)
+
+From here, paste the URL for the json file associated with the library.
+Libraries can be found [here](https://docs.wpilib.org/en/stable/docs/software/vscode-overview/3rd-party-libraries.html#libraries)
